cf766abe97a351f58d063d7cdd9b0175ba6e1076
Yevhenii Smirnov <yevhenii.smirnov@actito.com>
2025-05-07 16:13:31 +0100

    feat(nse): add module

---
+++ ActitoNotificationServiceExtensionKit/Sources/ActitoNotificationServiceExtensionKit.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2025 Actito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ActitoNotificationServiceExtension.
+FOUNDATION_EXPORT double ActitoNotificationServiceExtensionVersionNumber;
+
+//! Project version string for ActitoNotificationServiceExtension.
+FOUNDATION_EXPORT const unsigned char ActitoNotificationServiceExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActitoNotificationServiceExtensionKit/PublicHeader.h>
