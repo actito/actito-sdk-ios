f0e9eff26db38d6435f2535f86fe540cf6cd8178
Yevhenii Smirnov <yevhenii.smirnov@actito.com>
2025-05-16 16:16:39 +0100

    feat(core): detect when the device was removed remotely

--- ActitoKit/Sources/Internals/Modules/ActitoDeviceModuleImpl.swift
+++ ActitoKit/Sources/Internals/Modules/ActitoDeviceModuleImpl.swift
@@ -51,7 +51,31 @@ internal class ActitoDeviceModuleImpl: NSObject, ActitoModule, ActitoDeviceModul
         if let storedDevice {
             let isApplicationUpgrade = storedDevice.appVersion != Bundle.main.applicationVersion
 
-            try await updateDevice()
+            do {
+                try await updateDevice()
+            } catch {
+                if case let ActitoNetworkError.validationError(response, _, _) = error, response.statusCode == 404 {
+                    logger.warning("The device was removed from Actito. Recovering...")
+
+                    logger.debug("Resetting local storage.")
+                    try await resetLocalStorage()
+
+                    logger.debug("Creating a new device")
+                    try await createDevice()
+                    hasPendingDeviceRegistrationEvent = true
+
+                    // Ensure a session exists for the current device.
+                    try await Actito.shared.session().launch()
+
+                    // We will log the Install & Registration events here since this will execute only one time at the start.
+                    try? await Actito.shared.eventsImplementation().logApplicationInstall()
+                    try? await Actito.shared.eventsImplementation().logApplicationRegistration()
+
+                    return
+                }
+
+                throw error
+            }
 
             // Ensure a session exists for the current device.
             try await Actito.shared.session().launch()
@@ -74,6 +98,28 @@ internal class ActitoDeviceModuleImpl: NSObject, ActitoModule, ActitoDeviceModul
             try? await Actito.shared.eventsImplementation().logApplicationInstall()
             try? await Actito.shared.eventsImplementation().logApplicationRegistration()
         }
+    }
+
+    private func resetLocalStorage() async throws {
+        for module in ActitoInternals.Module.allCases {
+            if let instance = module.klass?.instance {
+                logger.debug("Resetting module: \(module)")
+
+                do {
+                    try await instance.clearStorage()
+                } catch {
+                    logger.debug("Failed to reset '\(module)'.", error: error)
+                    throw error
+                }
+            }
+        }
+
+        try await Actito.shared.database.clear()
+
+        // Should only clear device-related local storage properties.
+        LocalStorage.device = nil
+        LocalStorage.preferredLanguage = nil
+        LocalStorage.preferredRegion = nil
     }
 
     internal func postLaunch() async throws {
