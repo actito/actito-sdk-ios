ffc59b9a1cf869444a47d07b0f6a361dfe5a3d01
João Ferreira <joao@notifica.re>
2025-05-28 14:45:08 +0100

    feat(utilities): add InsertSorted extension

---
+++ ActitoUtilitiesKit/Sources/Collections/Arrays.swift
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2025 Actito. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+
+    @discardableResult
+    public mutating func insertSorted(
+        _ element: Element,
+        by areInIncreasingOrder: (_ lhs: Element, _ rhs: Element) -> Bool
+    ) -> Int {
+        let index = firstIndex(where: { areInIncreasingOrder(element, $0) }) ?? count
+        insert(element, at: index)
+
+        return index
+    }
+}
