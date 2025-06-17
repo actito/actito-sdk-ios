c6a16befb0ac4a66ba353bcefa609b1d1f93f66e
João Ferreira <joao.ferreira@actito.com>
2025-06-17 10:06:41 +0100

    refactor(assets): move from ActitoModule to ActitoLaunchComponent

---
+++ ActitoAssetsKit/Sources/Internals/LaunchComponent.swift
@@ -0,0 +1,39 @@
+//
+// Copyright (c) 2025 Actito. All rights reserved.
+//
+
+import ActitoKit
+
+internal class LaunchComponent: NSObject, ActitoLaunchComponent{
+    internal static let instance = LaunchComponent()
+
+    internal let implementation = ActitoAssetsImpl.instance
+
+    internal func migrate() {
+        // no-op
+    }
+
+    internal func configure() {
+        logger.hasDebugLoggingEnabled = Actito.shared.options?.debugLoggingEnabled ?? false
+    }
+
+    internal func clearStorage() async throws {
+        // no-op
+    }
+
+    internal func launch() async throws {
+        // no-op
+    }
+
+    internal func postLaunch() async throws {
+        // no-op
+    }
+
+    internal func unlaunch() async throws {
+        // no-op
+    }
+
+    internal func executeCommand(_ command: String, data: Any?) async throws -> Any?{
+        return nil
+    }
+}
