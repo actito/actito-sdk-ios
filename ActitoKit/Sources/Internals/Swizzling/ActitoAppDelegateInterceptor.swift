b5fc2d4a681cd209f09168a0a4a0cce3f508744f
Yevhenii Smirnov <yevhenii.smirnov@actito.com>
2025-09-01 17:38:37 +0100

    refactor(core): annotate app delegate interceptor protocol with MainActor

--- ActitoKit/Sources/Internals/Swizzling/ActitoAppDelegateInterceptor.swift
+++ ActitoKit/Sources/Internals/Swizzling/ActitoAppDelegateInterceptor.swift
@@ -5,23 +5,19 @@
 import UIKit
 
 @objc
+@MainActor
 public protocol ActitoAppDelegateInterceptor {
 //    @objc optional func applicationDidBecomeActive(_ application: UIApplication)
 //
 //    @objc optional func applicationWillResignActive(_ application: UIApplication)
 
-    @MainActor
     @objc optional func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)
 
-    @MainActor
     @objc optional func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error)
 
-    @MainActor
     @objc optional func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void)
 
-    @MainActor
     @objc optional func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool
 
-    @MainActor
     @objc optional func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool
 }
