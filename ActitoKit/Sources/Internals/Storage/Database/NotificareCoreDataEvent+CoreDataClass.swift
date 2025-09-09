9c4e18a9e956adf64170d052ffad46fcab0d6aca
João Ferreira <joao.ferreira@actito.com>
2025-09-09 11:56:56 +0100

    refactor: manually generate CoreData object classes

---
+++ ActitoKit/Sources/Internals/Storage/Database/NotificareCoreDataEvent+CoreDataClass.swift
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2025 Actito. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+internal typealias NotificareCoreDataEventCoreDataClassSet = NSSet
+
+@objc(NotificareCoreDataEvent)
+internal class NotificareCoreDataEvent: NSManagedObject {}
