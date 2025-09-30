e889c086f297ea7d79b2a81a0178aaa606563e8b
Yevhenii Smirnov <yevhenii.smirnov@actito.com>
2025-09-30 12:43:06 +0100

    chore(utilities): update logger implementation

---
+++ ActitoUtilitiesKit/Sources/Internals/ActitoUtilities.swift
@@ -0,0 +1,9 @@
+//
+// Copyright (c) 2025 Actito. All rights reserved.
+//
+
+public struct ActitoUtilities {
+    public static func configureLogger(hasDebugLoggingEnabled: Bool) {
+        logger.hasDebugLoggingEnabled = hasDebugLoggingEnabled
+    }
+}
