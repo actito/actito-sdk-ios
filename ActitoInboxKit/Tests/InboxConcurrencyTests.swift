6c4083f23a562011fb841fd14b6432e61946a3c6
João Ferreira <joao.ferreira@actito.com>
2026-03-18 08:44:21 +0000

    test(inbox): refactor inbox concurrency tests to be able to be run automatically

--- ActitoInboxKit/Tests/InboxConcurrencyTests.swift
+++ ActitoInboxKit/Tests/InboxConcurrencyTests.swift
@@ -2,17 +2,18 @@
 // Copyright (c) 2025 Actito. All rights reserved.
 //
 
-import Testing
-import ActitoKit
+import UserNotifications
+@testable import ActitoKit
 @testable import ActitoInboxKit
+import ActitoTestSupportKit
+import Testing
 
 @MainActor
+@Suite(.serialized, ActitoConfigurationTrait(workflow: .launch, mode: .perSuite))
 internal struct InboxConcurrencyTests {
 
     @Test
     internal func testMassiveRefreshOperations() async throws {
-        try await setupActito()
-
         await withThrowingTaskGroup(of: Void.self) { group in
             for _ in 0...10 {
                 group.addTask {
@@ -24,7 +25,14 @@ internal struct InboxConcurrencyTests {
 
     @Test
     internal func testMassiveOpenOperations() async throws {
-        try await setupActito()
+        Actito.shared.notificationCenter = MockNotificationCenter()
+        Actito.shared.inbox().notificationCenter = MockNotificationCenter()
+
+        try await Actito.shared.events().logCustom("test_massive_open_operation")
+
+        while Actito.shared.inbox().items.isEmpty {
+            try await Actito.shared.inbox().refresh()
+        }
 
         let item = try #require(Actito.shared.inbox().items.first(where: { !$0.opened }))
 
@@ -38,19 +46,5 @@ internal struct InboxConcurrencyTests {
 
         let updatedItem = try #require(Actito.shared.inbox().items.first(where: { $0.id == item.id }))
         #expect(updatedItem.opened == true)
-    }
-
-    private func setupActito() async throws {
-        Actito.shared.configure(
-            servicesInfo: ActitoServicesInfo(
-                applicationKey: "",
-                applicationSecret: ""
-            ),
-            options: ActitoOptions(
-                debugLoggingEnabled: true
-            )
-        )
-
-        try await Actito.shared.launch()
     }
 }
